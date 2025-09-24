90daba32529bece1c049550de2cec801f5392198
Isabelypb <isabely.porto.barbosa@escola.pr.gov.br>
2025-09-24 08:56:56 -0300

    Add files via upload
    formulario de escola

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import csv
 import os
+from flask import request
 
 app = Flask(__name__)
 
@@ -14,7 +15,7 @@ def salvar_escola(Nome_da_instituiçao, Tipo_de_instituiçao, INEP, CNPJ, Telefo
             escritor.writerow([ 'Nome_da_instituiçao', 'Tipo_de_instituiçao', 'INEP', 'CNPJ', 'Telefone', 'Email', 'Localidade', 'Turno_de_funcionamento', 'Nivel_de_escolaridade'])
         escritor.writerow([Nome_da_instituiçao, Tipo_de_instituiçao, INEP, CNPJ, Telefone, Email, Localidade, Turno_de_funcionamento, Nivel_de_escolaridade])
 
-def ler_cadastro():
+def ler_escola():
     dados = []
     if os.path.isfile(ARQUIVO_CSV):
         with open(ARQUIVO_CSV, mode="r", encoding='utf-8') as file:
@@ -24,10 +25,43 @@ def ler_cadastro():
     return dados
 
 @app.route("/")
-def index ():
+def index():
     return render_template("index.html")
 
 @app.route("/")
 def sobre():
     return render_template("sobre.html")
 
+@app.route("/cadastrar_escola")
+def cadastrar():
+    salvar_escola("CEEP","publico", "41129857", "78.146.230/0001-50", "(41) 3284-6820", "ceep@ceep.net.br", "Curitiba, Paraná", "Manha", "ensino medio")
+    return "Escola cadastrada com sucesso!"
+
+@app.route("\listar_escola")
+def listar():
+    escola = ler_escola()
+    resposta = "<h1>Lista de escolas</h1><ul>"
+    for p in escola:
+        resposta += f"<li>{p['Nome_da_instituiçao']} ({p['Tipo_de_instituiçao']}) - {p['INEP']} ({p['CNPJ']}) - {p['Telefone']} ({p['Email']}) - {p['Localidade']} ({p['Turno_de_funcionamento']}) - {p['Nivel_de_escolaridade']}<li>"
+        resposta += "</ul>"
+        return resposta
+    
+@app.route("/cadastrar_form_escola")
+def cadastrar_form_escola():
+    Nome_da_instituiçao = request.args.get(" Nome_da_instituiçao")
+    Tipo_de_instituiçao = request.args.get("Tipo_de_instituiçao")
+    INEP = request.args.get("INEP")
+    CNPJ = request.args.get("CNPJ")
+    Telefone = request.args.get("Telefone")
+    Email = request.args.get("Email")
+    Localidade = request.args.get("Localidade")
+    Turno_de_funcionamento = request.args.get("Turno_de_funcionamento")
+    Nivel_de_escolaridade = request.args.get("Nivel_de_escolaridade ")
+    
+    salvar_escola(Nome_da_instituiçao, Tipo_de_instituiçao, INEP, CNPJ, Telefone, Email, Localidade, Turno_de_funcionamento, Nivel_de_escolaridade)
+    
+    return f"Escola cadastrada com sucesso: {Nome_da_instituiçao} ({Tipo_de_instituiçao}) - {INEP} ({CNPJ}) - {Telefone} ({Email}) - {Localidade} ({Turno_de_funcionamento}) - {Nivel_de_escolaridade}"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
